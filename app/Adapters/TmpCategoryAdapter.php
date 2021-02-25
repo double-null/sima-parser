4d0771a00346cd27d88dea1fcb536b65a2ae16e2
double-null <double-null@yandex.ua>
2021-02-25 17:33:10 +0300

    Upgrade temp category adapter

---
+++ app/Adapters/TmpCategoryAdapter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Adapters;
+
+class TmpCategoryAdapter
+{
+    private $categories;
+
+    public static function factory()
+    {
+        return new self();
+    }
+
+    public function setCategories($categories) {
+        $this->categories = $categories;
+        return $this;
+    }
+
+    public function run()
+    {
+        $out = [];
+        foreach ($this->categories as $category) {
+            $out[] = [
+                'alien_id' => $category->id,
+                'name' => $category->name,
+                'level' => $category->level,
+                'path' => $category->path,
+            ];
+        }
+        return $out;
+    }
+}
